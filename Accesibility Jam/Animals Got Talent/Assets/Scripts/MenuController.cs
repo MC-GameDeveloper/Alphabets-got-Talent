6d538192dfe7835532157b85d1f3e546d4fece2e
MT_CENT <mtc9417@gmail.com>
2018-09-14 18:53:32 -0400

    Initial Setup

---
+++ Accesibility Jam/Animals Got Talent/Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+
+public class MenuController : MonoBehaviour {
+
+	void Update(){
+
+		if(Input.GetKeyDown(KeyCode.E)){
+
+			SceneManager.LoadScene ("Tutorial");
+
+		}
+		if(Input.GetKeyDown(KeyCode.T)){
+
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
