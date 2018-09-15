e1738c807edf35697c724d725fbd5c650084008c
MT_CENT <mtc9417@gmail.com>
2018-09-15 19:54:21 -0400

    end of day 2

--- Accesibility Jam/Alphabets Got Talent/Assets/Scripts/Text.cs
+++ Accesibility Jam/Alphabets Got Talent/Assets/Scripts/Text.cs
@@ -9,6 +9,21 @@ public class Text : MonoBehaviour {
 	private InputField text;
 	public GameObject elephantPrefabRef;
 	private GameObject elephantInstance;
+	public GameObject tigerPrefabRef;
+	private GameObject tigerInstance;
+	public GameObject sheepPrefabRef;
+	private GameObject sheepInstance;
+	public GameObject alligatorPrefabRef;
+	private GameObject alligatorInstance;
+	public GameObject iguanaPrefabRef;
+	private GameObject iguanaInstance;
+	public GameObject monkeyPrefabRef;
+	private GameObject monkeyInstance;
+	public GameObject narwhalPrefabRef;
+	private GameObject narwhalInstance;
+	public GameObject owlPrefabRef;
+	private GameObject owlInstance;
+
 	public float animalSpeed;
 
 
@@ -37,11 +52,6 @@ public class Text : MonoBehaviour {
 
 			if (text.text== "t") {
 
-				SceneManager.LoadScene ("Tutorial");
-				text.text = "";
-			}
-			if (text.text== "e") {
-
 				SceneManager.LoadScene ("Main");
 				text.text = "";
 			}
@@ -65,6 +75,55 @@ public class Text : MonoBehaviour {
 			elephantInstance = Instantiate (elephantPrefabRef);
 
 		
+			text.text = "";
+
+		}
+		if (text.text== "t") {
+			tigerInstance = Instantiate (tigerPrefabRef);
+
+
+			text.text = "";
+
+		}
+		if (text.text== "s") {
+			sheepInstance = Instantiate (sheepPrefabRef);
+
+
+			text.text = "";
+
+		}
+		if (text.text== "a") {
+			alligatorInstance = Instantiate (alligatorPrefabRef);
+
+
+			text.text = "";
+
+		}
+		if (text.text== "i") {
+			iguanaInstance = Instantiate (iguanaPrefabRef);
+
+
+			text.text = "";
+
+		}
+		if (text.text== "m") {
+			monkeyInstance = Instantiate (monkeyPrefabRef);
+
+
+			text.text = "";
+
+		}
+		if (text.text== "n") {
+			narwhalInstance = Instantiate (narwhalPrefabRef);
+
+
+			text.text = "";
+
+		}
+		if (text.text== "o") {
+			owlInstance = Instantiate (owlPrefabRef);
+
+
 			text.text = "";
 
 		}
